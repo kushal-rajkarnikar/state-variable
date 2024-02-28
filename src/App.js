9098dca6f3cd28444a7668249abd15ce39cd852b
kushal-rajkarnikar <kushal.rajkarnikar@gmail.com>
2024-02-28 13:17:16 +0545

    Initial Push

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
 import "./App.css";
 import React from "react";
-import Star from "./Star";
 
 export default function App() {
-  const [contact, setContact] = React.useState({
-    firstName: "John",
-    lastName: "Doe",
-    phone: "+1 (719) 555-1212",
-    email: "itsmyrealname@example.com",
-    isFavorite: true,
-  });
+  const [counter, setCounter] = React.useState(0);
 
-  function toggleFavorite() {
-    setContact((prevContact) => ({
-      ...prevContact,
-      isFavorite: !prevContact.isFavorite,
-    }));
+  function handleAdd() {
+    setCounter((prevCounter) => prevCounter + 1);
   }
 
+  function handleSubtract() {
+    setCounter((prevCounter) => prevCounter - 1);
+  }
   return (
     <main>
-      <article className="card">
-        <img src="./images/avatar.png" className="card--image" />
-        <div className="card--info">
-          <Star isFilled={contact.isFavorite} handleClick={toggleFavorite} />
-          <h2 className="card--name">
-            {contact.firstName} {contact.lastName}
-          </h2>
-          <p className="card--contact">{contact.phone}</p>
-          <p className="card--contact">{contact.email}</p>
-        </div>
-      </article>
+      <button onClick={handleSubtract}>Subtract</button>
+      <div className="circle">{counter}</div>
+      <button onClick={handleAdd}>Add</button>
     </main>
   );
 }
